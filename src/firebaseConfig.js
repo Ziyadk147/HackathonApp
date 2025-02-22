fee7445644d31fb0d2bb5475a837b7796f55ace2
Alionit01 <fotopob33@gmail.com>
2025-02-22 18:19:16 +0500

    edit firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import Firebase
+import { initializeApp } from "firebase/app";
+
+// Your Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyC2JCt3nqUNpufjyFgquYXTGftww-Lltfk",
+    authDomain: "micathon-9c73d.firebaseapp.com",
+    projectId: "micathon-9c73d",
+    storageBucket: "micathon-9c73d.firebasestorage.app",
+    messagingSenderId: "1046448162841",
+    appId: "1:1046448162841:web:ccd33e43b51d5b31c5c288",
+    measurementId: "G-CL2Q3YJD2W"
+  };
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
